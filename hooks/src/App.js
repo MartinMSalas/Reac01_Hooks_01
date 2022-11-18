39629ea03c2f1f151502d331be8777e1ebe14561
MartinMSalas <ingmartinmsalas@gmail.com>
2022-11-18 11:35:48 -0300

    adding files

---
+++ hooks/src/App.js
@@ -0,0 +1,32 @@
+import './App.css';
+import React, { useState } from 'react';
+
+function App() {
+  const [count, setCount] = useState(4);
+  
+  function decrementCount(){
+    setCount(prevCount=>
+      prevCount - 1
+    );
+  }
+
+  function incrementCount(){
+    setCount(preCount => {
+      
+      return preCount + 1 
+    }
+      )
+  }
+
+
+
+  return (
+    <>
+      <button onClick={decrementCount}>-</button>
+      <span>{count}</span>
+      <button onClick={incrementCount}>+</button>
+    </>
+  );
+}
+
+export default App;
